84ef8df0b4e8ce9d2de3d5c197d7042c8f15cad2
Nijiko Yonskai <me@nijikokun.com>
2011-06-27 00:13:21 -0400

    Fixed those weird characters...

--- SyntaxHighlighter/shBrush@todo.js
+++ SyntaxHighlighter/shBrush@todo.js
@@ -10,19 +10,19 @@
  * https://github.com/Nijikokun/.todo
  */
 SyntaxHighlighter.brushes.Custom = function(){
-  var funcs       = /\B(@todo|@unfinished|@uncompleted)/gmi;
-  var keywords    = /\B(@done|@finished|@completed)/gmi;
-  var operators   = /\B(@soon|@in-progress|@currently)/gmi;
- 
-  this.regexList = [
-      { regex: /\((.*)\)$/gm,                    css: 'color2' },
-      { regex: /--(.*)$/gm,                      css: 'comments' },
-      { regex: /((^|\s)#(\w+)|(^|\s)#)/g,        css: 'color1' },
-      { regex: new RegExp(funcs),                css: 'keyword' },
-      { regex: new RegExp(operators),            css: 'variable' },
-      { regex: new RegExp(keywords),             css: 'value' }
-      ];
+    var funcs = /\B(@todo|@unfinished|@uncompleted)/gmi;
+    var keywords = /\B(@done|@finished|@completed)/gmi;
+    var operators = /\B(@soon|@in-progress|@currently)/gmi;
+
+    this.regexList = [
+        { regex: /\((.*)\)$/gm,                       css: 'color2' },
+        { regex: /--(.*)$/gm,                         css: 'comments' },
+        { regex: /((^|\s)#(\w+)|(^|\s)#)/g,           css: 'color1' },
+        { regex: new RegExp(funcs),                   css: 'keyword' },
+        { regex: new RegExp(operators),               css: 'variable' },
+        { regex: new RegExp(keywords),                 css: 'value' }
+    ];
 };
 
 SyntaxHighlighter.brushes.Custom.prototype = new SyntaxHighlighter.Highlighter();
-SyntaxHighlighter.brushes.Custom.aliases  = ['todo', '@todo', '.todo', '2do', '.td', '@td', 'td'];
+SyntaxHighlighter.brushes.Custom.aliases = ['todo', '@todo', '.todo', '2do', '.td', '@td', 'td'];
